1a29058c1e23bf2f8ab6d4f5238facab1b51a895
João D. Moreira <joao@jdmoreira.me>
2018-11-03 12:10:56 +0100

    Add a callbacker for each UIViewController lifecycle signal

--- Presentation/UIViewController+Signals.swift
+++ Presentation/UIViewController+Signals.swift
@@ -11,110 +11,112 @@ import Flow
 import UIKit
 
 public extension UIViewController {
-    var viewDidLoadSignal: Signal<UIView> {
-        return signal(for: #selector(UIViewController.viewDidLoad))
+    var viewDidLoadSignal: Signal<Void> {
+        return Signal(callbacker: callbackers.viewDidLoad)
     }
 
-    var viewWillAppearSignal: Signal<UIView> {
-        return signal(for: #selector(UIViewController.viewWillAppear(_:)))
+    var viewWillAppearSignal: Signal<Bool> {
+        return Signal(callbacker: callbackers.viewWillAppear)
     }
 
-    var viewDidAppearSignal: Signal<UIView> {
-        return signal(for: #selector(UIViewController.viewDidAppear(_:)))
+    var viewWillDisappearSignal: Signal<Bool> {
+        return Signal(callbacker: callbackers.viewWillDisappear)
     }
 
-    var viewWillDisappearSignal: Signal<UIView> {
-        return signal(for: #selector(UIViewController.viewWillDisappear(_:)))
-    }
-    var viewDidDisappearSignal: Signal<UIView> {
-        return signal(for: #selector(UIViewController.viewDidDisappear(_:)))
+    var viewDidDisappearSignal: Signal<Bool> {
+        return Signal(callbacker: callbackers.viewDidDisappear)
     }
 
-    var viewWillLayoutSubviewsSignal: Signal<UIView> {
-        return signal(for: #selector(UIViewController.viewWillLayoutSubviews))
+    var viewWillLayoutSubviewsSignal: Signal<Void> {
+        return Signal(callbacker: callbackers.viewWillLayoutSubviews)
     }
 
-    var viewDidLayoutSubviewsSignal: Signal<UIView> {
-        return signal(for: #selector(UIViewController.viewDidLayoutSubviews))
-    }
-
-    private func signal(`for` selector: Selector) -> Signal<UIView> {
-        return Signal(callbacker: callbacker)
-            .filter(predicate: { return $0 == selector })
-            .map { _ in return self.view }
+    var viewDidLayoutSubviewsSignal: Signal<Void> {
+        return Signal(callbacker: callbackers.viewDidLayoutSubviews)
     }
 }
 
-/// The callbacker used by the swizzled implementations
-private var callbackerKey = false
+/// A callbacker box used by the swizzled implementations
+private class Callbackers {
+    let viewDidLoad = Callbacker<Void>()
+    let viewWillAppear = Callbacker<Bool>()
+    let viewDidAppear = Callbacker<Bool>()
+    let viewWillDisappear = Callbacker<Bool>()
+    let viewDidDisappear = Callbacker<Bool>()
+    let viewWillLayoutSubviews = Callbacker<Void>()
+    let viewDidLayoutSubviews = Callbacker<Void>()
+}
+
+private var callbackersKey = false
 private extension UIViewController {
-    var callbacker: Callbacker<Selector> {
+    var callbackers: Callbackers {
         let _ = UIViewController.runOnce
-        return associatedValue(forKey: &callbackerKey, initial: Callbacker<Selector>())
+        return associatedValue(forKey: &callbackersKey, initial: Callbackers())
     }
+}
+
+/// Swizzling of all UIViewController lifecycle methods
+extension UIViewController {
+    static let runOnce: Void = {
+        selectorsToSwizzle.forEach { (original, swizzled) in
+            swizzle(class: UIViewController.self, original: original, swizzled: swizzled)
+        }
+    }()
+
+    static let selectorsToSwizzle: [(original: Selector, swizzled: Selector)] = [
+        (#selector(UIViewController.viewDidLoad), #selector(UIViewController._swizzled_viewDidLoad)),
+        (#selector(UIViewController.viewWillAppear(_:)), #selector(UIViewController._swizzled_viewWillAppear(_:))),
+        (#selector(UIViewController.viewDidAppear(_:)), #selector(UIViewController._swizzled_viewDidAppear(_:))),
+        (#selector(UIViewController.viewWillDisappear(_:)), #selector(UIViewController._swizzled_viewWillDisappear(_:))),
+        (#selector(UIViewController.viewDidDisappear(_:)), #selector(UIViewController._swizzled_viewDidDisappear(_:))),
+        (#selector(UIViewController.viewWillLayoutSubviews), #selector(UIViewController._swizzled_viewWillLayoutSubviews)),
+        (#selector(UIViewController.viewDidLayoutSubviews), #selector(UIViewController._swizzled_viewDidLayoutSubviews)) ]
+}
+
+private func swizzle(class aClass: AnyClass, original: Selector, swizzled: Selector) {
+    guard let original = class_getInstanceMethod(aClass, original),
+        let swizzled = class_getInstanceMethod(aClass, swizzled) else {
+        assertionFailure("Invalid selector for " +  String(describing: aClass))
+        return
+    }
+
+    method_exchangeImplementations(original, swizzled)
 }
 
 /// Swizzled method implementations
 private extension UIViewController {
     @objc func _swizzled_viewDidLoad() {
         self._swizzled_viewDidLoad()
-        callbacker.callAll(with: #selector(UIViewController.viewDidLoad))
+        callbackers.viewDidLoad.callAll()
     }
 
     @objc func _swizzled_viewWillAppear(_ animated: Bool) {
         self._swizzled_viewDidAppear(animated)
-        callbacker.callAll(with: #selector(UIViewController.viewWillAppear(_:)))
+        callbackers.viewWillAppear.callAll(with: animated)
     }
 
     @objc func _swizzled_viewDidAppear(_ animated: Bool) {
         self._swizzled_viewDidAppear(animated)
-        callbacker.callAll(with: #selector(UIViewController.viewDidAppear(_:)))
+        callbackers.viewDidAppear.callAll(with: animated)
     }
 
     @objc func _swizzled_viewWillDisappear(_ animated: Bool) {
         self._swizzled_viewWillDisappear(animated)
-        callbacker.callAll(with: #selector(UIViewController.viewWillDisappear(_:)))
+        callbackers.viewWillDisappear.callAll(with: animated)
     }
 
     @objc func _swizzled_viewDidDisappear(_ animated: Bool) {
         self._swizzled_viewDidDisappear(animated)
-        callbacker.callAll(with: #selector(UIViewController.viewDidDisappear(_:)))
+        callbackers.viewDidDisappear.callAll(with: animated)
     }
 
     @objc func _swizzled_viewWillLayoutSubviews() {
         self._swizzled_viewWillLayoutSubviews()
-        callbacker.callAll(with: #selector(UIViewController.viewWillLayoutSubviews))
+        callbackers.viewWillLayoutSubviews.callAll()
     }
 
     @objc func _swizzled_viewDidLayoutSubviews() {
         self._swizzled_viewDidLayoutSubviews()
-        callbacker.callAll(with: #selector(UIViewController.viewDidLayoutSubviews))
-    }
-}
-
-
-/// Swizzling of all UIViewController lifecycle methods
-extension UIViewController: Swizzable { }
-extension UIViewController {
-    static let runOnce: Void = {
-        let swizzle = UIViewController.swizzle
-        swizzle(#selector(UIViewController.viewDidLoad), #selector(UIViewController._swizzled_viewDidLoad))
-        swizzle(#selector(UIViewController.viewWillAppear(_:)), #selector(UIViewController._swizzled_viewWillAppear(_:)))
-        swizzle(#selector(UIViewController.viewDidAppear(_:)), #selector(UIViewController._swizzled_viewDidAppear(_:)))
-        swizzle(#selector(UIViewController.viewWillDisappear(_:)), #selector(UIViewController._swizzled_viewWillDisappear(_:)))
-        swizzle(#selector(UIViewController.viewDidDisappear(_:)), #selector(UIViewController._swizzled_viewDidDisappear(_:)))
-        swizzle(#selector(UIViewController.viewWillLayoutSubviews), #selector(UIViewController._swizzled_viewWillLayoutSubviews))
-        swizzle(#selector(UIViewController.viewDidLayoutSubviews), #selector(UIViewController._swizzled_viewDidLayoutSubviews))
-    }()
-}
-
-/// Helper protocol for swizzling
-protocol Swizzable: AnyObject { }
-
-extension Swizzable {
-    static func swizzle(_ originalSel: Selector, _ swapSel: Selector) {
-        let original = class_getInstanceMethod(Self.self, originalSel)
-        let new = class_getInstanceMethod(Self.self, swapSel)
-        method_exchangeImplementations(original!, new!)
+        callbackers.viewDidLayoutSubviews.callAll()
     }
 }
