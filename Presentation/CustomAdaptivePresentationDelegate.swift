00fe05d5f48d5e53949dd17edeb6667b551a30d3
Martin Andonoski <mandonoski@paypal.com>
2023-11-20 09:34:14 +0100

    Mistake

--- Presentation/CustomAdaptivePresentationDelegate.swift
+++ Presentation/CustomAdaptivePresentationDelegate.swift
@@ -22,6 +22,7 @@ public final class CustomAdaptivePresentationDelegate: NSObject, UIAdaptivePrese
 
     // swiftlint:disable large_tuple
     public typealias WillPresentAdaptivelyInput = (UIPresentationController, UIModalPresentationStyle, UIViewControllerTransitionCoordinator?)
+    // swiftlint:enable large_tuple
 
     public var willPresentSignal: Signal<WillPresentAdaptivelyInput> {
         return Signal(callbacker: willPresentCallbacker)
